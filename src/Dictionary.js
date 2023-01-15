0a60066716d6839455fc7668a78f5ae55c9129b7
Olha <olunchik.ivanova@gmail.com>
2023-01-15 22:06:04 +0000

    Update on Photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "563492ad6f9170000100000190893ad402a24c2ca2a78e0755ecd76e";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
